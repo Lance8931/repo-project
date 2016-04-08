d8bc2a65e4db47f8d08d5a4fbbe7687070b57b41
L.J.H <1838133252@qq.com>
2016-04-08 17:56:40 +0800

    添加Maven模板项目

---
+++ TemplateProject/src/main/java/com/template/Demo.java
@@ -0,0 +1,12 @@
+package com.template;
+
+/**
+ *
+ *
+ * 
+ */
+public class Demo {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ TemplateProject/src/test/java/com/template/DemoTest.java
@@ -0,0 +1,16 @@
+package com.template;
+
+import org.junit.Test;
+
+/**
+ *
+ *
+ * 
+ */
+public class DemoTest {
+
+	@Test
+	public void test() {
+	}
+
+}
