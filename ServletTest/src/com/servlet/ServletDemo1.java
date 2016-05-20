c6eda86e773219afd201de1b9d4188ebd0f902ab
L.J.H <1838133252@qq.com>
2016-05-20 17:56:41 +0800

    add：Servlet项目

---
+++ ServletTest/src/com/servlet/ServletDemo1.java
@@ -0,0 +1,64 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDemo1
+ */
+@WebServlet(urlPatterns = { "/ServletDemo1*", "/ServletDemo2" }, initParams = { @WebInitParam(name = "kkk", value = "323") })
+public class ServletDemo1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private ServletConfig config;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletDemo1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		this.config = config;
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(this.config.getInitParameter("kkk"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
